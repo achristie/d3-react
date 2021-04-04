4da4a24ae66dfe1e94b1a4f8acbd8421686181f4
Andrew Christie <awchristie86@gmail.com>
2021-04-04 18:01:02 -0400

    first commit

---
+++ pages/home.js
@@ -0,0 +1,11 @@
+/**
+ * @class       : home
+ * @author      : awc (awc@$HOSTNAME)
+ * @created     : Sunday Apr 04, 2021 17:58:52 EDT
+ * @description : home
+ */
+
+export default function Home() {
+  return <h1> Hello! </h1>;
+}
+
