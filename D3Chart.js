74ff7f4399fba8fb93f39e992647dd919fc2f5ed
Andrew Christie <awchristie86@gmail.com>
2021-04-07 21:09:39 -0400

    section 4 pt 1

--- D3Chart.js
+++ D3Chart.js
@@ -12,59 +12,90 @@ const width = 500 - margin.left - margin.right;
 
 export default class D3Chart {
   constructor(element) {
-    const svg = d3
+    this.svg = d3
       .select(element)
       .append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom);
 
-    const g = svg
+    this.g = this.svg
       .append("g")
       .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
+    this.svg
+      .append("text")
+      .attr("x", width / 2)
+      .attr("y", height + 60)
+      .attr("text-anchor", "middle")
+      .text("People");
+
+    this.svg
+      .append("text")
+      .attr("x", 10)
+      .attr("y", height / 2)
+      .text("Heights (cm)");
+    this.xAxisGroup = this.g
+      .append("g")
+      .attr("transform", `translate(0,${height})`);
+    this.yAxisGroup = this.g.append("g");
+
     d3.json("./api/test").then((data) => {
-      const rect = g.selectAll("rect").data(data);
-
-      const x = d3
-        .scaleBand()
-        .paddingInner(0.2)
-        .domain(data.map((d) => d.name))
-        .range([0, width]);
-      const y = d3
-        .scaleLinear()
-        .domain([0, 1.05 * d3.max(data, (d) => d.height)])
-        .range([height, 0]);
-
-      rect
-        .join("rect")
-        .attr("fill", "cyan")
-        .attr("x", (d, i) => x(d.name))
-        .attr("y", (d) => y(d.height))
-        .attr("width", x.bandwidth())
-        .attr("height", (d) => height - y(d.height));
-
-      const xAxisGroup = g
-        .append("g")
-        .attr("transform", `translate(0,${height})`);
-      const yAxisGroup = g.append("g");
-      const xAxis = d3.axisBottom(x);
-      const yAxis = d3.axisLeft(y);
-      xAxisGroup.call(xAxis);
-      yAxisGroup.call(yAxis);
-
-      svg
-        .append("text")
-        .attr("x", width / 2)
-        .attr("y", height + 60)
-        .attr("text-anchor", "middle")
-        .text("People");
-
-      svg
-        .append("text")
-        .attr("x", 10)
-        .attr("y", height / 2)
-        .text("Heights (cm)");
+      this.data = data;
+      let gender = ["male", "female"];
+      let count = 1;
+      this.update(gender[0]);
+      d3.interval(() => {
+        this.update(gender[count % 2]);
+        count++;
+      }, 2000);
     });
+  }
+
+  update(gender) {
+    const data = this.data.filter((d) => d.gender == gender);
+    const t = d3.transition(800);
+    const x = d3
+      .scaleBand()
+      .paddingInner(0.2)
+      .domain(data.map((d) => d.name))
+      .range([0, width]);
+    const y = d3
+      .scaleLinear()
+      .domain([0, 1.05 * d3.max(data, (d) => d.height)])
+      .range([height, 0]);
+
+    const rect = this.g.selectAll("rect").data(data);
+
+    rect.join(
+      (enter) =>
+        enter
+          .append("rect")
+          .attr("fill", "cyan")
+          .attr("x", (d, i) => x(d.name))
+          .attr("y", height)
+          .attr("width", x.bandwidth())
+          .call((enter) =>
+            enter
+              .transition(t)
+              .attr("y", (d) => y(d.height))
+              .attr("height", (d) => height - y(d.height))
+          ),
+      (update) =>
+        update.call((update) =>
+          update
+            .transition(t)
+            .attr("x", (d) => x(d.name))
+            .attr("y", (d) => y(d.height))
+            .attr("width", x.bandwidth())
+            .attr("height", (d) => height - y(d.height))
+        ),
+      (exit) => exit.transition(t).attr("height", 0).attr("y", height).remove()
+    );
+
+    const xAxis = d3.axisBottom(x);
+    const yAxis = d3.axisLeft(y);
+    this.xAxisGroup.transition(t).call(xAxis);
+    this.yAxisGroup.transition(t).call(yAxis);
   }
 }
 
--- pages/api/test.js
+++ pages/api/test.js
@@ -6,11 +6,17 @@
  */
 
 const data = [
-  { name: "Andrew", height: 173 },
-  { name: "Jimmy", height: 180 },
-  { name: "Deb", height: 160 },
-  { name: "Taylor", height: 175 },
-  { name: "Lorena", height: 178 },
+  { name: "Andrew", height: 173, gender: "male" },
+  { name: "Jimmy", height: 180, gender: "male" },
+  { name: "Deb", height: 160, gender: "female" },
+  { name: "Taylor", height: 175, gender: "female" },
+  { name: "Lorena", height: 178, gender: "female" },
+  { name: "Jordan", height: 171, gender: "male" },
+  { name: "Frank", height: 185, gender: "male" },
+  { name: "Patrick", height: 181, gender: "male" },
+  { name: "Kelly", height: 179, gender: "female" },
+  { name: "Rocio", height: 173, gender: "female" },
+  { name: "Sara", height: 163, gender: "female" },
 ];
 
 export default (req, res) => {
